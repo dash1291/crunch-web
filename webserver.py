d2a3a3130e51ab8772e3d1a55246c3783379fd8b
Ashish Dubey <ashish.dubey91@gmail.com>
2013-03-03 21:12:49 +0530

    use template to genrate client.py code

--- webserver.py
+++ webserver.py
@@ -1,44 +1,54 @@
 import tornado.ioloop
 import tornado.web
+from tornado.template import Template
 
 from crunch.crunchauth import add_account, get_user
 
 class index(tornado.web.RequestHandler):
     def get(self):
-        self.render('templates/index.html')
+        self.render('templates/index.html', code=None)
 
 class register(tornado.web.RequestHandler):
-	def get(self):
-		self.render('templates/register.html')
+    def get(self):
+        self.render('templates/register.html', error=None)
 
-	def post(self):
-		error = None
-		try:
-			username = self.request.arguments['username'][0]
-			if len(username) == 0:
-				raise Exception('bad username')
+    def post(self):
+        error = None
+        try:
+            username = self.request.arguments['username'][0]
+            if len(username) == 0:
+                raise Exception('bad username')
 
-			if get_user(username) == True:
-				error = 'Username already exists.'
-				raise Exception()
-		except:
-			if not error == None:
-				error = 'A non-zero length username is required.'
+            """if get_user(username) == True:
+                error = 'Username already exists.'
+                raise Exception()"""
+        except:
+            if not error == None:
+                error = 'A non-zero length username is required.'
 
-		try:
-			password = self.request.arguments['password'][0]
-			if len(password) == 0:
-				raise Exception('bad password')
-		except:
-			if not error == None:
-				error = 'A non-zero length password is required.'
-		print username
-		add_account(username, password)
-		self.render('templates/register.html', error=error)
+        try:
+            password = self.request.arguments['password'][0]
+            if len(password) == 0:
+                raise Exception('bad password')
+        except:
+            if not error == None:
+                error = 'A non-zero length password is required.'
+        print username
+        """add_account(username, password)"""
+
+        if error == None:
+            t = Template(open('templates/client_sample.html',
+                'r').read())
+            client_sample_code = t.generate(username=username,
+                password=password)
+
+            self.render('templates/index.html', code=client_sample_code)
+        else:
+            self.render('templates/register.html', error=error)
 
 if __name__ == "__main__":
     application = tornado.web.Application([
-    	(r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
+        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
         (r"/", index),
         (r"/register/", register),
     ])
